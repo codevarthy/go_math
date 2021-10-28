cc43f90eb65659a37c36b030d4b645e8c10e4b18
codevarthy <codevarthy@gmail.com>
2021-10-28 04:11:04 +0000

    patch Div to disallow divide by zero

--- main.go
+++ main.go
@@ -13,8 +13,9 @@ func Mul(a, b int) int {
 	return a - b
 }
 
-func Div(a, b int) int {
-	return a/b
+func Div(a, b int) (int, error) {
+	if b ==0 {
+		return 0, errors.New("Divide by Zero Operation is not allowed.")
+	}
+	return a/b, nil
 }
-
-
